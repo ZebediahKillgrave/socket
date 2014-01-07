edff528b798940662a43971961692c85e6192b45
Florent Espanet <florent.esp@gmail.com>
2014-01-07 13:29:21 +0100

    unix socket done

--- Unix/Socket.cpp
+++ Unix/Socket.cpp
@@ -1,45 +1,47 @@
 #if defined(linux) || defined(__linux)
 
+#include <sys/types.h>
+#include <sys/socket.h>
 #include "Socket.hpp"
 
 int Socket::socket(int domain, int type, int protocol)
 {
-  return (0);
+  return (socket(domain, type, protocol));
 }
 
 int Socket::connect(int sockfd, const void *addr, int addrlen)
 {
-  return (0);
+  return (connect(sockfd, (const struct sockaddr *)addr, (socklen_t)addrlen));
 }
 
 int Socket::send(int sockfd, const void *buf, int len, int flags)
 {
-  return (0);
+  return (send(sockfd, buf, (size_t)len, flags));
 }
 
 int Socket::receive(int sockfd, void *buf, int len, int flags)
 {
-  return (0);
+  return (recv(sockfd, buf, (size_t)len, flags));
 }
 
 int Socket::bind(int sockfd, const void *addr, int addrlen)
 {
-  return (0);
+  return (bind(sockfd, (const struct sockaddr *)addr, (socklen_t)addrlen));
 }
 
 int Socket::listen(int sockfd, int backlog)
 {
-  return (0);
+  return (listen(sockfd, backlog));
 }
 
 int Socket::accept(int sockfd, void *addr, int *addrlen)
 {
-  return (0);
+  return (accept(sockfd, (struct sockaddr *)addr, addrlen));
 }
 
 int Socket::close(int sockfd)
 {
-  return (0);
+  return (close(sockfd));
 }
 
 #endif
